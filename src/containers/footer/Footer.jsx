725ccdb2b916e8059ae4516ccee742879c5cf9c8
jonathanvolker <joni_451@hotmail.com>
2022-02-13 23:58:48 -0300

    añadido form de contacto

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 import "./footer.css";
+import emailjs from 'emailjs-com';
+import contactoImg from "../../assets/contacto.png";
+import{ init } from '@emailjs/browser';
+init("user_0tL6wwgQ45ZoBhj4wlgg4");
 
 const Footer = () => {
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'user_0tL6wwgQ45ZoBhj4wlgg4')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
     <div className="rpaAutomations__footer section__padding">
       <div className="rpaAutomations__footer-heading">
@@ -9,7 +24,34 @@ const Footer = () => {
         Si querés potenciar tu transformación digital, dejanos un mensaje y un asesor se contactará de inmediato.
         </h1>
       </div>
-
+      <div className="rpaAutomations__footer-img-form">
+          <div>
+            <img src={contactoImg}alt="rpa-automations-footer-img" className="rpaAutomations__footer-img" />  
+          </div>
+         
+          <div className="rpaAutomations__footer-form"> 
+            <form  onSubmit={sendEmail}>
+                <div className="rpaAutomations__footer-form-div">
+                <label>Nombre</label>
+                <input type="text" name="user_name" />
+                </div>
+                <div className="rpaAutomations__footer-form-div">
+                <label>Telefono</label>
+                <input type="text" name="contact_number" />
+                </div>
+                <div className="rpaAutomations__footer-form-div"> 
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                </div >
+                <div className="rpaAutomations__footer-form-div">
+                <label>Mensaje</label>
+                <textarea name="message" />
+                </div>
+                <input className="rpaAutomations__footer-form-button" type="submit" value="Enviar" />
+            </form>
+          </div>
+      </div>   
+      
       <div className="rpaAutomations__footer-copyright">
         <p>© 2022 RPA Automations. derechos reservados.</p>
       </div>
